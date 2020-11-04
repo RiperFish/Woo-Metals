0df15f2051bb0047b99b09d81c3f0da655085dac
Sims <mustapha.benfetta@gmail.com>
2020-11-04 08:21:24 +0100

    theme is ready, navigation working

--- functions.php
+++ functions.php
@@ -249,3 +249,24 @@ function woocommerce_quantity_input($args = array(), $product = null, $echo = tr
         return $string;
     }
 }
+
+/**
+ * Change number of products that are displayed per page (shop page)
+ */
+add_filter('loop_shop_per_page', 'new_loop_shop_per_page', 20);
+
+function new_loop_shop_per_page($cols)
+{
+    // $cols contains the current number of products per page based on the value stored on Options –> Reading
+    // Return the number of products you wanna show per page.
+    $cols = 10;
+    return $cols;
+}
+
+/* DISPLAY PRODUCT IN SHOP PAGE BY DATE : YOU CAN CHANGE IT ANYTIME */
+add_filter('woocommerce_default_catalog_orderby', 'change_products_display_order');
+ 
+function change_products_display_order( $sort_by ) {
+	return 'date';
+}
+
